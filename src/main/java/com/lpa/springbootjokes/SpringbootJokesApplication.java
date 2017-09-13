01cec2cb50f3b5f5f684b28713f7e95394227b2f
L.P <parisot.laurent@gmail.com>
2017-09-13 09:40:39 +0200

    Go back to Java configuration

--- src/main/java/com/lpa/springbootjokes/SpringbootJokesApplication.java
+++ src/main/java/com/lpa/springbootjokes/SpringbootJokesApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-@ImportResource("classpath:chuck-config.xml")
+//@ImportResource("classpath:chuck-config.xml")
 public class SpringbootJokesApplication {
 
 	public static void main(String[] args) {
